99725162fa1f9f1cbdeac66862ccbaf4eea26cb0
flpmcruz <flpmireles@gmail.com>
2023-01-18 15:26:02 -0500

    first

---
+++ js/app.js
@@ -0,0 +1,46 @@
+
+
+const modalImagen = document.querySelector('#modal-imagen')
+
+if(modalImagen){
+    modalImagen.addEventListener('show.bs.modal', function(event){
+        const enlace = event.relatedTarget;
+        const rutaImagen = enlace.getAttribute('data-bs-imagen')
+    
+        //construir imagen
+        const imagen = document.createElement('img')
+        imagen.src = `img/${rutaImagen}.jpg`
+        imagen.classList.add('img-fluid')
+        imagen.alt = 'Imagen Galeria'
+    
+        const contenidoModal = document.querySelector('.modal-body')
+        contenidoModal.append(imagen)
+        console.log(imagen)
+    })
+    
+    modalImagen.addEventListener('hidden.bs.modal', function(){
+        const contenidoModal = document.querySelector('.modal-body')
+        contenidoModal.textContent = ''
+    })
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
